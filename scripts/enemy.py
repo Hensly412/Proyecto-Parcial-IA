e9e3f33866ed9c43d81b92625def0ee6c5ccfcbb
Hensly412 <henslyvidal412@gmail.com>
2025-07-02 20:24:38 -0400

    probando

---
+++ scripts/enemy.py
@@ -0,0 +1,9 @@
+class Enemy:
+    def __init__(self, x, y):
+        # Solo movimiento hacia el jugador
+    
+    def update(self, dt, player_pos):
+        # Perseguir jugador directamente
+    
+    def render(self, screen):
+        # Dibujar enemigo
