d0a08267399fa3a4ac623dfa18cf1bc603ee6615
Hensly412 <henslyvidal412@gmail.com>
2025-07-05 21:13:39 -0400

    init finalizado

---
+++ Scripts/_init_.py
@@ -0,0 +1,24 @@
+# Paquete scripts - HV Warriors
+# Autor: Hensly Manuel Vidal Rosario
+# Matrícula: 23-MISN-2-007
+
+"""
+Paquete que contiene todos los módulos del juego HV Warriors.
+
+Módulos incluidos:
+- config: Configuración del juego
+- game: Lógica principal del juego
+- player: Clase del jugador
+- enemy: Clase del enemigo con IA
+- bullet: Clase de proyectiles
+- menu: Menú principal
+- game_map: Mapa del juego
+- sound_manager: Gestor de audio
+- sprite_manager: Gestor de sprites
+- behavior_tree: Árbol de comportamiento (implementado desde cero)
+- astar: Algoritmo A* (implementado desde cero)
+"""
+
+__version__ = "1.0.0"
+__author__ = "Hensly Manuel Vidal Rosario"
+__email__ = "23-MISN-2-007"
